a7183c7cf2236d1a1e34e987b3f57227827a20ae
sathishvit1996@gmail.com <sathishvit040496@gmail.com>
2026-01-08 19:31:56 +0530

    .

---
+++ Numpy_exp.py
@@ -0,0 +1,6 @@
+import Numpy_exp as np
+#import pandas as pd
+
+A = np.array([1,2,3])
+
+print(A)
--- numpy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-class playgroud :
-    playgroud= 11 
-
-    def virat 
